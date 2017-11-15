371d1a8415a9770beed6d7c58210494daff84591
Aryeh Armon <aryeh.armon@gmail.com>
2017-11-15 10:44:42 +0200

    added next to inpout

--- nodes/core/io/21-httpin.js
+++ nodes/core/io/21-httpin.js
@@ -196,11 +196,11 @@ module.exports = function(RED) {
                 var msgid = RED.util.generateId();
                 res._msgid = msgid;
                 if (node.method.match(/^(post|delete|put|options|patch)$/)) {
-                    node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.body});
+                    node.send({_msgid:msgid,req:req,next:next,res:createResponseWrapper(node,res),payload:req.body});
                 } else if (node.method == "get") {
-                    node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.query});
+                    node.send({_msgid:msgid,req:req,next:next,res:createResponseWrapper(node,res),payload:req.query});
                 } else {
-                    node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res)});
+                    node.send({_msgid:msgid,req:req,next:next,res:createResponseWrapper(node,res)});
                 }
             };
 
