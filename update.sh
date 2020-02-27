f3523d431e678a7857cde34070ad14c290177241
aryeh <aryeh@luwfy.com>
2020-02-27 17:06:32 +0200

    update versaion 1.07

---
+++ update.sh
@@ -0,0 +1,7 @@
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/@node-red/editor-api && npm publish
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/@node-red/editor-client && npm publish
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/@node-red/nodes && npm publish
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/@node-red/registry && npm publish
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/@node-red/runtime && npm publish
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/@node-red/util && npm publish
+cd /home/aryeh/Code/aaaaaaaaassssssss/packages/node_modules/node-red && npm publis
